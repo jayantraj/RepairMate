8c9c624028cd6430d3498669251cad66cff4e67d
Jayantraj Coimbatore Selvakumar <41659842+jayantraj@users.noreply.github.com>
2024-05-23 17:41:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Install Python dependencies from requirements.txt
+pip install -r requirements.txt
+
 # Install Detectron2
 pip install 'git+https://github.com/facebookresearch/detectron2.git'
