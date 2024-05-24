ab4935dc66379e7f0f5275428a97c591eba91df5
Jayantraj Coimbatore Selvakumar <41659842+jayantraj@users.noreply.github.com>
2024-05-23 21:33:04 -0500

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install Python dependencies from requirements.txt
-pip install -r requirements.txt
-
-# Install Detectron2
-pip install 'git+https://github.com/facebookresearch/detectron2.git'
